83b678861c0ac6bcb62764d624bdc2f76b0df2da
Casey Carroll <caseydean8@gmail.com>
2022-07-01 14:59:41 -0400

    Created mongoose models

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-require('dotenv').config();
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: process.env.NODE_ENV === 'development'
-}))
+// Connect to database
+connectDB();
 
-app.listen(port, console.log(`Server up on port ${port}`))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server up on port ${port}`));
