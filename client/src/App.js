a5afd2ae683308e83501569652afedd6ab7a8b09
Casey Carroll <caseydean8@gmail.com>
2022-07-02 17:45:46 -0400

    Fixed apollo cache warning

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,30 @@ import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+// Merge function added handle cache data warning in console from delete client handling in ClientRow.jsx line 13
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+          projects: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  // cache: new InMemoryCache(), replaced with cache variable
+  cache,
 });
 
 function App() {
@@ -13,7 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="Container">
-        <Clients />
+          <Clients />
         </div>
       </ApolloProvider>
     </>
--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,24 +1,38 @@
-import {FaTrash} from 'react-icons/fa';
-import { useMutation } from '@apollo/client';
-import { DELETE_ClIENT } from '../mutations/ClientMutations';
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_ClIENT } from "../mutations/ClientMutations";
+// Use refresh page after client delete
+import { GET_CLIENTS } from "../queries/ClientQueries";
 
-export default function ClientRow({client}) {
-  const [deleteClient ] = useMutation(DELETE_ClIENT, {
+export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_ClIENT, {
     variables: { id: client.id },
-  })
-  return (
-  <tr>
-    <td>{client.name}</td>
-    <td>{client.email}</td>
-    <td>{client.phone}</td>
-    <td>
-      <button className="btn btn-danger btn-sm"
-      onClick={deleteClient}
-      >
-     <FaTrash /> 
-    </button>
-    </td>
-  </tr>
-  )
+    // A way to refresh view after deleting client but...
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    // ... instead of making a call to the server, go to the cache and write the last GET_CLIENTS query without the client with deleteClient id.
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({
+        query: GET_CLIENTS,
+      });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
+  });
 
+  return (
+    <tr>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
 }
