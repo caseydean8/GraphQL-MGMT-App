5031e65fdd52983f3089feabeb7a5a47310f3fde
Casey Carroll <caseydean8@gmail.com>
2022-07-02 16:24:12 -0400

    Fetch and display clients

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import ClientRow from "./ClientRow"
 // The convention here is to use all caps, and syntax for gql is backticks instead of parentheses. Also query syntax is the same from GraphiQL.
 const GET_CLIENTS = gql`
   {
@@ -13,10 +14,30 @@ const GET_CLIENTS = gql`
 
 export default function Clients() {
   // ApolloProvider is essentially it's own state manager, loading state is true or false.
-  const { loading, error, data } = useQuery(GET_CLIENTS)
+  const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>Loading . . .</p>
-  if (error) return <p>Something went wrong</p>
+  if (loading) return <p>Loading . . .</p>;
+  if (error) return <p>Something went wrong</p>;
 
-  return <>{!loading && !error && <h1>Clients</h1>}</>
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map(client => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
