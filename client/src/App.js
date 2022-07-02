3187da85c3c4827ac4352bcb04ae10760f3454de
Casey Carroll <caseydean8@gmail.com>
2022-07-02 18:48:00 -0400

    Add client mutation added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import AddClientModal from "./components/AddClientModal";
 
-// Merge function added handle cache data warning in console from delete client handling in ClientRow.jsx line 13
+// Merge function added handle cache data warning in console from delete client handling in ClientRow.jsx line 13 and add client handling in AddClientModal.jsx line 14
 const cache = new InMemoryCache({
   typePolicies: {
     Query: {
@@ -34,6 +35,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="Container">
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
