e2539ea1a286dbb1ec38b41e4d861f210ccb7660
Casey Carroll <caseydean8@gmail.com>
2022-07-03 16:21:20 -0400

    Set up React router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Clients from "./components/Clients";
-import Projects from "./components/Projects";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 // Merge function added handle cache data warning in console from delete client handling in ClientRow.jsx line 13 and add client handling in AddClientModal.jsx line 14
 const cache = new InMemoryCache({
@@ -34,12 +35,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
