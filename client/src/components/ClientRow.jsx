0050bbc49b3ed1cbf7cd17677d8005627d08c25b
Casey Carroll <caseydean8@gmail.com>
2022-07-05 18:06:01 -0400

    Deleting client deletes client projects

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -3,24 +3,25 @@ import { useMutation } from "@apollo/client";
 import { DELETE_ClIENT } from "../mutations/ClientMutations";
 // Use refresh page after client delete
 import { GET_CLIENTS } from "../queries/ClientQueries";
+import { GET_PROJECTS } from "../queries/ProjectQueries";
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_ClIENT, {
     variables: { id: client.id },
     // A way to refresh view after deleting client but...
-    // refetchQueries: [{ query: GET_CLIENTS }],
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
     // ... instead of making a call to the server, go to the cache and write the last GET_CLIENTS query without the client with deleteClient id.
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({
-        query: GET_CLIENTS,
-      });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({
+    //     query: GET_CLIENTS,
+    //   });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
